f5535463ff29171225fc74933c5f853c0846058c
leapsrn <RichardNye@hotmail.co.uk>
2018-07-06 10:34:42 +0100

    Push test

--- code/Agent_framework/agents/RemoteControl.py
+++ code/Agent_framework/agents/RemoteControl.py
@@ -13,3 +13,5 @@ def cube_moved(evt):
 def ControlWhite1(evt, robot: cozmo.robot.Robot):
     robot.add_event_handler(cozmo.objects.EvtObjectAppeared, handle_object_appeared)
     robot.add_event_handler(cozmo.objects.EvtObjectDisappeared, handle_object_disappeared)
+
+    ##This is a push test to see the timings
