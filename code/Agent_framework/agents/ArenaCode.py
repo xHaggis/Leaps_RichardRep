d43933242f90a0976cd8cd24600475b4f5fc8d76
leapsrn <RichardNye@hotmail.co.uk>
2018-07-13 11:22:37 +0100

    Arena Code Update

--- code/Agent_framework/agents/ArenaCode.py
+++ code/Agent_framework/agents/ArenaCode.py
@@ -15,46 +15,41 @@ assignedNumber = 1
 enemyNumber = 2
 robot = cozmo.robot.Robot
 
-'''''''''
-def ArenaCode(evt, **kwargs):
-    look_around = robot.start_behavior(cozmo.behavior.BehaviorTypes.LookAroundInPlace)
-
-    # try to find a block
-    cube = None
-
-    try:
-        cube = robot.world.wait_for_observed_light_cube(timeout=30)
-        print("Found cube", cube)
-
-    except asyncio.TimeoutError:
-        print("Didn't find a cube :-(")
-
-    finally:
-        # whether we find it or not, we want to stop the behavior
-        look_around.stop()
-
-    if cube is None:
-        robot.play_anim_trigger(cozmo.anim.Triggers.MajorFail)
-        return
-
-    if cube is not None:
-        robot.pickup_object(self,)
-
-    print("Yay, found cube")
-
-    robot.turn_in_place(degrees(90)).wait_for_completed()
-
-    action = robot.place_object_on_ground_here(cube)
-    print("got action", action)
-    result = action.wait_for_completed(timeout=30)
-    print("got action result", result)
-''''
-
 def ArenaCode(evt, **kwargs):
     gameInProgress = True
 
-    while gameInProgress = True:
-        
+    while gameInProgress == True:
+        look_around = robot.start_behavior(cozmo.behavior.BehaviorTypes.LookAroundInPlace)
+
+        # try to find a block
+        cube = None
+
+        try:
+            cube = robot.world.wait_for_observed_light_cube(timeout=30)
+            print("Found cube", cube)
+
+        except asyncio.TimeoutError:
+            print("Didn't find a cube :-(")
+
+        finally:
+            # whether we find it or not, we want to stop the behavior
+            look_around.stop()
+
+        if cube is None:
+            robot.play_anim_trigger(cozmo.anim.Triggers.MajorFail)
+            return
+
+        if cube is not None:
+            robot.pickup_object(self, )
+
+        print("Yay, found cube")
+
+        robot.turn_in_place(degrees(90)).wait_for_completed()
+
+        action = robot.place_object_on_ground_here(cube)
+        print("got action", action)
+        result = action.wait_for_completed(timeout=30)
+        print("got action result", result)
 
     if homeBase = 98:
 
