86e062214c4f9189642b809531b6b6a87977aeb2
leapsrn <RichardNye@hotmail.co.uk>
2018-07-17 14:36:30 +0100

    Arena Code Update

--- code/Agent_framework/agents/ArenaCode.py
+++ code/Agent_framework/agents/ArenaCode.py
@@ -15,10 +15,35 @@ assignedNumber = 1
 enemyNumber = 2
 robot = cozmo.robot.Robot
 
-def ArenaCode(evt, **kwargs):
-    gameInProgress = True
+'''''
+robot: cozmo.robot.Robot
+self, evt, **kwargs
+'''''
+
+def ArenaCode(self, evt, **kwargs):
+    gameInProgress =  True
 
     while gameInProgress == True:
+
+        cozmo.connect(self.run_cozmo)
+
+        def on_new_camera_image(self, event, *, image: cozmo.world.CameraImage, **kw):
+            raw_image = image.raw_image
+            self.img2 = cv2.cvtColor(np.array(raw_image), cv2.COLOR_RGB2BGR)
+            self.check_image()
+
+        async def set_up_cozmo(self, coz_conn):
+            self._robot = await coz_conn.wait_for_robot()
+            self._robot.camera.image_stream_enabled = True
+            self._robot.add_event_handler(cozmo.world.EvtNewCameraImage, self.on_new_camera_image)
+            self._robot.set_head_angle(cozmo.util.Angle(degrees=0))
+            return 1
+
+        async def run_cozmo(self, coz_conn):
+            await self.set_up_cozmo(coz_conn)
+            while True:
+                await asyncio.sleep(0)
+
         look_around = robot.start_behavior(cozmo.behavior.BehaviorTypes.LookAroundInPlace)
 
         # try to find a block
@@ -51,7 +76,7 @@ def ArenaCode(evt, **kwargs):
         result = action.wait_for_completed(timeout=30)
         print("got action result", result)
 
-    if homeBase = 98:
+
 
     robot.drive_wheels(50)
 
